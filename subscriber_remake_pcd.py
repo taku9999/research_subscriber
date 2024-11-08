3f9d6feff6ce76d6ce1e804852e5d73824d553dd
taku9999 <102945088+taku9999@users.noreply.github.com>
2024-11-08 12:10:46 +0000

    [update] PointCloud2とCustomMsgに両対応

--- subscriber_remake_pcd.py
+++ subscriber_remake_pcd.py
@@ -3,6 +3,8 @@
 
 import rospy
 from livox_ros_driver.msg import CustomMsg
+from sensor_msgs.msg import PointCloud2
+import sensor_msgs.point_cloud2 as pc2
 import open3d as o3d
 import numpy as np
 import datetime
@@ -19,6 +21,8 @@ LOG_DIRS = "/workspace/bind_data"
 
 PUBLISH_HZ = 60
 TARGET_HZ = 10
+
+MSG_TYPE = 0 # [0]=PointCloud2, [1]=CustomMsg
 
 
 def init_cuda():
@@ -58,21 +62,35 @@ def callback_lidar(point_cloud, args):
     que = args[0]
     ros_time = args[1]
     
-    num_points = point_cloud.point_num # Pointの数
-    
     # 共有メモリにROS_timeを保存
-    timestamp = point_cloud.header.stamp
-    ros_time.value = float(str(timestamp.secs) + "." + str(timestamp.nsecs).zfill(9)[:3])
+    ros_time_raw = point_cloud.header.stamp
+    ros_time_unix = float(str(ros_time_raw.secs) + "." + str(ros_time_raw.nsecs).zfill(9)[:3])
+    ros_time_dt = datetime.datetime.utcfromtimestamp(ros_time_unix)
+    ros_time_dt_jst = ros_time_dt + datetime.timedelta(hours=9)
+    ros_time_str = ros_time_dt_jst.strftime('%Y-%m-%d_%H-%M-%S_%f')[:-3]
+    ros_time.value = bytes(ros_time_str, "utf-8") # 共有メモリを更新(str)
 
-    # tensorを用いて点群をpcdに保存
-    points = np.zeros((num_points, 3), dtype="float32")
-    intensities = np.zeros((num_points, 1), dtype="float32")
-    for i, point in enumerate(point_cloud.points):
-        points[i, 0] = point.x
-        points[i, 1] = point.y
-        points[i, 2] = point.z
-        intensities[i, 0] = np.float32(point.reflectivity)
+    if MSG_TYPE == 0:
+        # PointCloud2型の点群データを読み取る
+        points = []
+        intensities = []
+        for point in pc2.read_points(point_cloud, field_names=("x", "y", "z", "intensity"), skip_nans=True):
+            points.append([point[0], point[1], point[2]])
+            intensities.append([point[3]])
+        points = np.array(points, dtype="float32")
+        intensities = np.array(intensities, dtype="float32")
+    elif MSG_TYPE == 1:
+        # CustomMsg型の点群データを読み取る
+        num_points = point_cloud.point_num # Pointの数
+        points = np.zeros((num_points, 3), dtype="float32")
+        intensities = np.zeros((num_points, 1), dtype="float32")
+        for i, point in enumerate(point_cloud.points):
+            points[i, 0] = point.x
+            points[i, 1] = point.y
+            points[i, 2] = point.z
+            intensities[i, 0] = np.float32(point.reflectivity)
     
+    # tensorを用いて点群をpcdに保存
     pcd_t = o3d.t.geometry.PointCloud(DEVICE)
     pcd_t.point.positions = o3d.core.Tensor(points, DTYPE, DEVICE)
     pcd_t.point.intensity = o3d.core.Tensor(intensities, DTYPE, DEVICE)
@@ -82,15 +100,20 @@ def callback_lidar(point_cloud, args):
     print(" :: [ADD] Now queue size :" + str(que.qsize()))
 
 
-def process_ros(q_pointcloud, m_ros_time):
+def process_ros(q_pointcloud, g_ros_time):
     init_cuda()
     
     rospy.init_node("research_subscriber")
-    rospy.Subscriber("/livox/lidar", CustomMsg, callback_lidar, (q_pointcloud, m_ros_time))
+
+    if MSG_TYPE == 0:
+        rospy.Subscriber("/livox/lidar", PointCloud2, callback_lidar, (q_pointcloud, g_ros_time))
+    elif MSG_TYPE == 1:
+        rospy.Subscriber("/livox/lidar", CustomMsg, callback_lidar, (q_pointcloud, g_ros_time))
+
     rospy.spin()
 
 
-def process_pcd(q_pointcloud, m_ros_time, pcd_filepass):
+def process_pcd(q_pointcloud, g_ros_time, pcd_filepass):
     coupling_num = PUBLISH_HZ / TARGET_HZ
     
     while 1:
@@ -107,7 +130,7 @@ def process_pcd(q_pointcloud, m_ros_time, pcd_filepass):
                     break
         
             print("=== Que Get Success !! ===")
-            o3d.t.io.write_point_cloud(pcd_filepass + "/" + str(m_ros_time.value).replace(".", "_", 1) + ".pcd", pcd_t_save)
+            o3d.t.io.write_point_cloud(pcd_filepass + "/" + str(g_ros_time.value)[2:-1] + ".pcd", pcd_t_save)
 
 
 def main():
@@ -119,7 +142,7 @@ def main():
     q_pointcloud = manager.Queue()
     
     # 共有メモリの設定
-    m_ros_time = Value("d", 0.0)
+    m_ros_time = Array("c", b"0000-00-00_00-00-00_000")
     
     # PCDファイルパスの設定
     pcd_filepass = LOG_DIRS + "/" + str(get_device_time("conv_str") + "_" + str(TARGET_HZ) + "Hz")
